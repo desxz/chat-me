2ee0fa03e7037928b0319d11bb673e22d1ef577d
desxz <muratgun545@gmail.com>
2021-02-21 14:05:33 +0300

    Room Structure Configurations

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,9 +1,36 @@
-app.controller('chatController', ['$scope', ($scope) =>{
+app.controller('chatController',['$scope', ($scope) => {
+    $scope.onlineList= [];
     $scope.activeTab = 2;
+    $scope.roomList = [];
+    $scope.chatClicked = false;
+    $scope.chatName = "";
+
+    const socket = io.connect('http://localhost:3000');
+    socket.on('onlineList', users => {
+        $scope.onlineList = users;
+        $scope.$apply();
+    });
+
+    socket.on('roomList', rooms => {
+        $scope.roomList = rooms;
+        $scope.$apply();
+    });
+
+    $scope.switchRoom = (room) => {
+        $scope.chatName = room.name;
+        $scope.chatClicked = true;
+    };
+
+    $scope.newRoom = () => {
+        //let randomName = Math.random().toString(36).substring(7);
+        let roomName = window.prompt('Enter Room Name');
+        if (roomName!==""){
+            socket.emit('newRoom', roomName);
+        }
+        
+    }
 
     $scope.changeTab = tab => {
         $scope.activeTab = tab;
-    }
-
-    const socket = io.connect('http://localhost:3000');
+    };
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -2,6 +2,11 @@ const socketio = require('socket.io');
 const io = socketio();
 const redisAdapter = require('socket.io-redis');
 const socketAuthorization = require('../middleware/socketAuthorization');
+const users = require('../models/users');
+const Users = require('./lib/Users');
+const Rooms = require('./lib/Rooms');
+
+
 
 const socketApi = {
     io
@@ -15,6 +20,36 @@ io.adapter(redisAdapter({
 
 io.on('connection', socket => {
     console.log(socket.request.user.name + ' connected!');
+    Users.upsert(socket.id, socket.request.user);
+
+    Users.list(users => {
+        io.emit('onlineList', users);
+    });
+
+    Rooms.list(rooms => {
+        console.log(rooms);
+        io.emit('roomList', rooms);
+    });
+
+    socket.on('newRoom', roomName => {
+        Rooms.upsert(roomName);
+        Rooms.list(rooms => {
+            console.log(rooms);
+            io.emit('roomList', rooms);
+        });
+    });
+
+    socket.on('disconnect', () => {
+        Users.remove(socket.request.user.googleId);
+
+        Users.list(users => {
+            io.emit('onlineList', users);
+        });
+        
+    });
+
+    
+
 });
 
 module.exports = socketApi;
